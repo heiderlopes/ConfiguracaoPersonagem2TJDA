6f482d1fe431ffe8b925fda0356991dd13171741
logonpf <logonpf@fiap.com.br>
2018-03-16 09:45:40 -0300

    Manipulacao de eventos

---
+++ app/src/main/java/com/example/logonpf/configuracaopersonagem/Personagem.kt
@@ -0,0 +1,14 @@
+package com.example.logonpf.configuracaopersonagem
+
+class Personagem() {
+
+    var nome: String = ""
+
+    fun andar() {
+
+    }
+
+    fun correr() {
+
+    }
+}
---
+++ app/src/main/java/com/example/logonpf/configuracaopersonagem/TipoPersonagemEnum.kt
@@ -0,0 +1,16 @@
+package com.example.logonpf.configuracaopersonagem
+
+enum class TipoPersonagemEnum constructor(val id: Int) {
+    ANAO(0),
+    ARQUEIRO(1),
+    BARDO(2),
+    ELFO(3),
+    GUERREIRO(4),
+    MAGO(5);
+
+    companion object {
+        fun from(findValue: Int): TipoPersonagemEnum =
+                TipoPersonagemEnum.values().first { it.id == findValue }
+    }
+
+}
